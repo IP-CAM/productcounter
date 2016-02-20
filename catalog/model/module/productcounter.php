48bd8073e923f87a961c752ceedf5b6c07920b28
KajeNick <KajeNick@gmail.com>
2016-02-20 22:55:06 +0300

    Add comment

--- catalog/model/module/productcounter.php
+++ catalog/model/module/productcounter.php
@@ -1,5 +1,8 @@
 <?php
 class ModelModuleProductcounter extends Model {
+    /**
+     * Getting count products by cat from DB
+     * */
 	public function getTotalByCat($cat_id){
 	   $query = $this->db->query("SELECT total FROM " . DB_PREFIX . "pcounter WHERE category_id = '" .(int)$cat_id. "'");
 
