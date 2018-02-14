c5b65f4830bd60262c0f3eff8841ad999eb88ed2
Nickolay Sukonny <kajenick@gmail.com>
2018-02-14 18:44:21 +0300

    identation

--- catalog/model/module/productcounter.php
+++ catalog/model/module/productcounter.php
@@ -3,13 +3,13 @@ class ModelModuleProductcounter extends Model {
     /**
      * Getting count products by cat from DB
      * */
-	public function getTotalByCat($cat_id){
-	   $query = $this->db->query("SELECT total FROM " . DB_PREFIX . "pcounter WHERE category_id = '" .(int)$cat_id. "'");
+    public function getTotalByCat($cat_id){
+       $query = $this->db->query("SELECT total FROM " . DB_PREFIX . "pcounter WHERE category_id = '" .(int)$cat_id. "'");
 
-	   if ($query->num_rows) {
-		  return $query->row['total'];
-	   }else{
-		  return 0;  
-	   }
-	}
+       if ($query->num_rows) { 
+          return $query->row['total'];
+       }else{
+          return 0;  
+       }
+    }
 }
